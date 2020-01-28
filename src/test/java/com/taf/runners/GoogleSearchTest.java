58d3825db471583c288fe8e18993385e4a8d6692
288225 <288225@LT105995.cts.com>
2020-01-28 18:15:08 +0530

    First Push for Google page automation

---
+++ src/test/java/com/taf/runners/GoogleSearchTest.java
@@ -0,0 +1,10 @@
+package com.taf.runners;
+
+import cucumber.api.CucumberOptions;
+import net.serenitybdd.cucumber.CucumberWithSerenity;
+import org.junit.runner.RunWith;
+
+@RunWith(CucumberWithSerenity.class)
+@CucumberOptions(features= {"src/test/resources/com/taf/features/googlesearch.feature"},glue = {
+"com.taf.steps"})
+public class GoogleSearchTest {}
